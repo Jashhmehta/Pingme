83e0ccf28afea20779bfe7d06ffc0e445848117d
Jashhmehta <jashmehtaa@gmail.com>
2024-05-26 11:27:40 +0530

    Added Search, Manage Groups icons and tooltips

--- src/Client/Components/Layout/Header.jsx
+++ src/Client/Components/Layout/Header.jsx
@@ -1,9 +1,32 @@
-import { AppBar, Box, IconButton, Toolbar, Typography } from "@mui/material";
+import {
+  AppBar,
+  Box,
+  IconButton,
+  Toolbar,
+  Tooltip,
+  Typography,
+} from "@mui/material";
 import React from "react";
-import { Menu as MenuIcon, Search as SearchIcon } from "@mui/icons-material";
+import {
+  Add as AddIcon,
+  Group as GroupIcon,
+  Menu as MenuIcon,
+  Search as SearchIcon,
+} from "@mui/icons-material";
+import { useNavigate } from "react-router-dom";
 const Header = () => {
+  const navigate = useNavigate();
   const handleMobile = () => {
     console.log("Mobile");
+  };
+  const openSearchDialog = () => {
+    console.log("Search Dialog");
+  };
+  const openNewGroup = () => {
+    console.log("Open new group");
+  };
+  const navigateToGroup = () => {
+    navigate("/groups")
   };
   return (
     <>
@@ -29,12 +52,33 @@ const Header = () => {
             <Box
               sx={{
                 flexGrow: 1,
-              }} 
+              }}
             />
 
             <Box>
-            <IconButton color="inherit" size="large" onClick={handleMobile} />
-
+              <Tooltip title="Search">
+                <IconButton
+                  color="inherit"
+                  size="large"
+                  onClick={openSearchDialog}
+                >
+                  <SearchIcon />
+                </IconButton>
+              </Tooltip>
+              <Tooltip title="New Group">
+                <IconButton color="inherit" size="large" onClick={openNewGroup}>
+                  <AddIcon />
+                </IconButton>
+              </Tooltip>
+              <Tooltip title="Manage Groups">
+                <IconButton
+                  color="inherit"
+                  size="large"
+                  onClick={navigateToGroup}
+                >
+                  <GroupIcon />
+                </IconButton>
+              </Tooltip>
             </Box>
           </Toolbar>
         </AppBar>
