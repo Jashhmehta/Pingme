003302a182cfccf93963c6d043bbb48bebe126d7
Jashhmehta <jashmehtaa@gmail.com>
2024-06-01 10:21:42 +0530

    Added Styling to NewGroup component

--- src/Client/Components/Specific/NewGroup.jsx
+++ src/Client/Components/Specific/NewGroup.jsx
@@ -12,22 +12,36 @@ import {
 } from "@mui/material";
 import { sampleUsers } from "../../../constants/sampleData";
 import UserItem from "../Shared/UserItem";
+import { useInputValidation } from "6pp";
 const NewGroup = () => {
+  const groupName=useInputValidation();
   const selectMemberHandler = () => {};
   return (
     <Dialog open>
-      <Stack p={{ xs: "1rem", sm: "2rem" }} width={"25rem"}>
-        <DialogTitle>New Group</DialogTitle>
-        <TextField />
-        <Typography>Members</Typography>
+      <Stack p={{ xs: "1rem", sm: "2rem" }} width={"25rem"} spacing={"2rem"}>
+        <DialogTitle textAlign={"center "} variant="h4">
+          New Group
+        </DialogTitle>
+        <TextField
+        label="Group Name"
+        value={groupName.value}
+        onChange={groupName.changeHandler}
+
+
+        
+        
+        />
+        <Typography variant="body1">Members</Typography>
         <Stack>
           {sampleUsers.map((i) => (
             <UserItem user={i} key={i._id} handler={selectMemberHandler} />
           ))}
         </Stack>
-        <Stack direction={"row"}>
-          <Button variant="text" color="error">Cancel</Button>
-          <Button variant="contained">Create</Button>
+        <Stack direction={"row"} justifyContent={"space-between"}>
+          <Button variant="text" color="error">
+            Cancel
+          </Button>
+          <Button variant="text">Create</Button>
         </Stack>
       </Stack>
     </Dialog>
