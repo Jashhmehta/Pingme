eccd19636ba610cef992bf7861bc3d5d98260d1a
Jashhmehta <jashmehtaa@gmail.com>
2024-06-01 10:40:01 +0530

    Updated NewGroups

--- src/Client/Components/Specific/NewGroup.jsx
+++ src/Client/Components/Specific/NewGroup.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Avatar,
   Button,
@@ -14,8 +14,17 @@ import { sampleUsers } from "../../../constants/sampleData";
 import UserItem from "../Shared/UserItem";
 import { useInputValidation } from "6pp";
 const NewGroup = () => {
-  const groupName=useInputValidation();
-  const selectMemberHandler = () => {};
+  const [members, setMembers] = useState(sampleUsers);
+  const [selectedmembers, setSelectedMembers] = useState([]);
+  const groupName = useInputValidation();
+  const selectMemberHandler = (id) => {
+    setSelectedMembers((prev) =>
+      prev.includes(id) ? prev.filter((currElement) => currElement !== id) : [...prev, id]
+    );
+  };
+  console.log(selectedmembers);
+
+  const submitHandler = () => {};
   return (
     <Dialog open>
       <Stack p={{ xs: "1rem", sm: "2rem" }} width={"25rem"} spacing={"2rem"}>
@@ -23,17 +32,13 @@ const NewGroup = () => {
           New Group
         </DialogTitle>
         <TextField
-        label="Group Name"
-        value={groupName.value}
-        onChange={groupName.changeHandler}
-
-
-        
-        
+          label="Group Name"
+          value={groupName.value}
+          onChange={groupName.changeHandler}
         />
         <Typography variant="body1">Members</Typography>
         <Stack>
-          {sampleUsers.map((i) => (
+          {members.map((i) => (
             <UserItem user={i} key={i._id} handler={selectMemberHandler} />
           ))}
         </Stack>
@@ -41,7 +46,9 @@ const NewGroup = () => {
           <Button variant="text" color="error">
             Cancel
           </Button>
-          <Button variant="text">Create</Button>
+          <Button variant="text" onClick={submitHandler}>
+            Create
+          </Button>
         </Stack>
       </Stack>
     </Dialog>
