bdb6c8e276912a114f61ee27df0029261d010c89
Jashhmehta <jashmehtaa@gmail.com>
2024-06-02 09:26:59 +0530

    Created a home component

--- src/Client/Pages/Home.jsx
+++ src/Client/Pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import AppLayout from '../Components/Layout/AppLayout'
-
+import { Box, Typography } from '@mui/material'
 const Home = () => {
   return (
-    <div>Home</div>
+    <Box bgcolor={"crimson"} height={"100%"}>
+    <Typography p={"2rem"} variant="h5" textAlign={"center"}> Select a friend to chat</Typography>
+    </Box>
   )
 }
 
