34a5441a310c816de730650cef9e6151c766a442
Jashhmehta <jashmehtaa@gmail.com>
2024-06-03 00:01:55 +0530

    Updated Groups component

--- src/Client/Pages/Groups.jsx
+++ src/Client/Pages/Groups.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import { Grid } from "@mui/material";
+import React from "react";
 
 const Groups = () => {
   return (
-    <div>Groups</div>
-  )
-}
+    <Grid container height={"100vh"}>
+      <Grid item sm={4} bgcolor={"bisque"}>
+        Groups List
+      </Grid>
+      <Grid
+        item
+        xs={12}
+        sm={8}
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          padding: "1rem 3rem",
+          position: "relative",
+        }}
+      >
+        {" "}
+        Group Details
+      </Grid>
+    </Grid>
+  );
+};
 
-export default Groups
+export default Groups;
