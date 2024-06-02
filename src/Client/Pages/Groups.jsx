be0315b1b1ff93bd0fc0e29260db92e3c6f68097
Jashhmehta <jashmehtaa@gmail.com>
2024-06-03 00:24:26 +0530

    Updated Groups

--- src/Client/Pages/Groups.jsx
+++ src/Client/Pages/Groups.jsx
@@ -1,11 +1,38 @@
-import { Grid } from "@mui/material";
+import { KeyboardBackspace } from "@mui/icons-material";
+import { Avatar, Grid, IconButton, Stack, Tooltip, Typography } from "@mui/material";
 import React from "react";
-
+import { useNavigate } from "react-router-dom";
+import { Link } from "../Components/Styles/StyledComponents";
+import AvatarCard from "../Components/Shared/AvatarCard"
 const Groups = () => {
+  const navigate = useNavigate();
+  const navigateBack = () => {
+    navigate("/");
+  };
+  const IconButtons = (
+    <>
+      <Tooltip titlee="back">
+        <IconButton
+          sx={{
+            position: "absolute",
+            left: "2rem",
+            top: "2rem",
+            bgcolor: "black",
+            color: "white",
+            ":hover": "rgba(0,0,0,0.7)",
+          }}
+          onClick={navigateBack}
+        >
+          <KeyboardBackspace />
+        </IconButton>
+      </Tooltip>
+    </>
+  );
+
   return (
     <Grid container height={"100vh"}>
       <Grid item sm={4} bgcolor={"bisque"}>
-        Groups List
+        <GroupsList />
       </Grid>
       <Grid
         item
@@ -19,11 +46,32 @@ const Groups = () => {
           position: "relative",
         }}
       >
-        {" "}
-        Group Details
+        {IconButtons}
       </Grid>
     </Grid>
   );
 };
 
+const GroupsList = ({ w = "100%", myGroups = [], chatId }) => {
+  <Stack>
+    {myGroups.length > 0 ? (
+      myGroups.map((group) => {})
+    ) : (
+      <Typography textAlign={"center"} padding={"1rem"}>
+        No Groups
+      </Typography>
+    )}
+  </Stack>;
+};
+const GroupListItem = ({ group, chatId }) => {
+  const { name, avatar, _id } = group;
+  return (
+    <Link>
+      <Stack>
+        <AvatarCard avatar={avatar}/>
+        <Typography>{name}</Typography>
+      </Stack>
+    </Link>
+  );
+};
 export default Groups;
