e5dc2521cf5669d3129b66ee4324998bea43449c
Jashhmehta <jashmehtaa@gmail.com>
2024-06-05 13:30:20 +0530

    Tweak: Added widget component

--- src/Client/Pages/Admin/Dashboard.jsx
+++ src/Client/Pages/Admin/Dashboard.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 import AdminLayout from "../../Components/Layout/AdminLayout";
 import { Box, Container, Paper, Stack, Typography } from "@mui/material";
-import { AdminPanelSettings, Notifications, Search } from "@mui/icons-material";
+import {
+  AdminPanelSettings,
+  Group,
+  Message,
+  Notifications,
+  Person,
+} from "@mui/icons-material";
 import moment from "moment/moment";
 import {
   CurveButton,
@@ -14,7 +20,6 @@ const Dashboard = () => {
       elevation={3}
       sx={{
         margin: "2rem 0",
-
         borderRadius: "1rem",
       }}
     >
@@ -43,15 +48,115 @@ const Dashboard = () => {
           {moment().format("dddd, D MMMM YYYY")}
         </Typography>
         <Notifications />
-
       </Stack>
     </Paper>
   );
+
+  const Widgets = (
+    <Stack
+      direction={{
+        xs: "column",
+        sm: "row",
+      }}
+      spacing={"2rem"}
+      justifyContent="space-between"
+      alignItems={"center"}
+      margin={"2rem 0"}
+    >
+      <Widget title={"Users"} value={34} Icon={<Person />} />
+      <Widget title={"Chats"} value={34} Icon={<Group />} />
+      <Widget title={"Messages"} value={690} Icon={<Message />} />
+    </Stack>
+  );
+
   return (
     <AdminLayout>
-      <Container component={"main"}>{Appbar}</Container>
+      <Container component={"main"}>
+        {Appbar}
+        {Widgets}
+        <Stack direction={"row"} spacing={"2rem"} flexWrap={"wrap"}>
+          <Paper
+            elevation={3}
+            sx={{
+              padding: "2rem 3.5rem",
+              borderRadius: "1rem",
+              width: "100%",
+              maxWidth: "45rem",
+              height: "25rem",
+            }}
+          >
+            <Typography margin={"2rem 0"} variant="h4">
+              Last Messages
+            </Typography>
+            {" chat"}
+          </Paper>
+          <Paper
+            elevation={3}
+            sx={{
+              padding: "1rem",
+              borderRadius: "1rem",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              width: { xs: "100%", sm: "50%" },
+              position: "relative",
+              maxWidth: "25rem",
+              height: "25rem",
+            }}
+          >
+            {"Doughnut Chart"}
+            <Stack
+              position={"absolute"}
+              direction={"row"}
+              justifyContent={"center"}
+              alignItems={"center"}
+              spacing={"0.5rem"}
+              width={"100%"}
+              height={"100%"}
+            >
+              <Group />
+              <Typography>Vs</Typography>
+              <Person />
+            </Stack>
+          </Paper>
+        </Stack>
+      </Container>
     </AdminLayout>
   );
 };
+
+const Widget = ({ title, value, Icon }) => (
+  <Paper
+    elevation={3}
+    sx={{
+      padding: "1rem",
+      margin: "2rem 0",
+      borderRadius: "1rem",
+      width: "20rem",
+      textAlign: "center",
+    }}
+  >
+    <Stack alignItems={"center"} spacing={"1rem"}>
+      <Typography
+        sx={{
+          color: "rgba(0,0,0,0.7)",
+          borderRadius: "50%",
+          border: `5px solid rgba(0,0,0,0.9)`,
+          width: "5rem",
+          height: "5rem",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        {value}
+      </Typography>
+      <Stack direction="row" alignItems="center" spacing="0.5rem">
+        {Icon}
+        <Typography variant="h6">{title}</Typography>
+      </Stack>
+    </Stack>
+  </Paper>
+);
 
 export default Dashboard;
