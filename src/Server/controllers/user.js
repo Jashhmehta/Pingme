94df0bf2103274a149bd58fc3fe4dd720e26559c
Jashhmehta <jashmehtaa@gmail.com>
2024-06-17 00:30:13 +0530

    Feature: Added accept request endpoint
    - This endpoint will be used to accept or reject friend request.

--- src/Server/controllers/user.js
+++ src/Server/controllers/user.js
@@ -4,8 +4,8 @@ import { cookieOptions, emitEvent, sendToken } from "../utils/features.js";
 import { TryCatch } from "../middelwares/error.js";
 import { ErrorHandler } from "../utils/utility.js";
 import { Chat } from "../models/chat.js";
-import {Request} from "../models/request.js";
-import {NEW_REQUEST} from "../constants/events.js"
+import { Request } from "../models/request.js";
+import { NEW_REQUEST, REFETCH_CHATS } from "../constants/events.js";
 
 //Login user and save the token in cookie
 const login = async (req, res, next) => {
@@ -78,24 +78,59 @@ const searchUser = async (req, res) => {
 const sendFriendRequest = TryCatch(async (req, res, next) => {
   const { userId } = req.body;
   const request = await Request.findOne({
-    $or:[
-      {sender:req.user, receiver:userId},
-      {sender:userId, receiver:req.user}
-    ]
+    $or: [
+      { sender: req.user, receiver: userId },
+      { sender: userId, receiver: req.user },
+    ],
   });
   if (request) return next(new ErrorHandler("Request already send", 400));
   await Request.create({
-    sender:req.user,
-    receiver:userId
+    sender: req.user,
+    receiver: userId,
   });
-  emitEvent(req,NEW_REQUEST,[userId])
+  emitEvent(req, NEW_REQUEST, [userId]);
   return res.status(200).json({
     success: true,
     message: "Friend request sent successfully",
   });
 });
 
-const acceptFriendRequest=TryCatch(async(req,res,next)=>{
-
-})
-export { login, register, getMyProfile, logout, searchUser, sendFriendRequest, acceptFriendRequest };
+const acceptFriendRequest = TryCatch(async (req, res, next) => {
+  const { requestId, accept } = req.body;
+  const request = await Request.findById(requestId)
+    .populate("sender", "name")
+    .populate("receiver", "name");
+  if (!request) return next(new ErrorHandler("Request not found", 404));
+  if (request.receiver.toString() !== req.user.toString())
+    return next(new ErrorHandler("Unauthorized", 401));
+  if (!accept) {
+    await Request.deleteOne();
+    return res.status(200).json({
+      success: true,
+      message: "Request deleted successfully",
+    });
+  }
+  const members = [request.sender._id, request.receiver._id];
+  await Promise.all([
+    Chat.create({
+      members,
+      name: `${request.sender.name}-${request.receiver.name}`,
+    }),
+    request.deleteOne(),
+  ]);
+  emitEvent(req, REFETCH_CHATS, members);
+  return res.status(200).json({
+    success: true,
+    message: "Friend request accepted",
+    senderId: request.sender._id,
+  });
+});
+export {
+  login,
+  register,
+  getMyProfile,
+  logout,
+  searchUser,
+  sendFriendRequest,
+  acceptFriendRequest,
+};
