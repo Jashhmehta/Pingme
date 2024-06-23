c76550455bef7d40f199481641987bb67092b4ff
Jashhmehta <jashmehtaa@gmail.com>
2024-06-23 19:56:50 +0530

    Feature: Integrated send request endpoint
    - Using this endpoint user can send friend request to other users.

--- src/Client/Components/Specific/Search.jsx
+++ src/Client/Components/Specific/Search.jsx
@@ -1,30 +1,38 @@
 import { useInputValidation } from "6pp";
+import { Search as SearchIcon } from "@mui/icons-material";
 import {
   Dialog,
   DialogTitle,
   InputAdornment,
+  List,
   Stack,
   TextField,
-  List,
 } from "@mui/material";
 import React, { useEffect, useState } from "react";
-import { Search as SearchIcon } from "@mui/icons-material";
-import UserItem from "../Shared/UserItem";
 import { useDispatch, useSelector } from "react-redux";
+import { useAsyncMutation } from "../../Hooks/hook";
+import {
+  useLazySearchUserQuery,
+  useSendFriendRequestMutation,
+} from "../../Redux/API/api";
 import { setIsSearch } from "../../Redux/reducers/misc";
-import { useLazySearchUserQuery } from "../../Redux/API/api";
+import UserItem from "../Shared/UserItem";
 
 const Search = () => {
   const { isSearch } = useSelector((state) => state.misc);
   const [searchUser, { data: searchResults }] = useLazySearchUserQuery();
+  const [sendFriendRequest, isLoadingSendFriendRequest] = useAsyncMutation(
+    useSendFriendRequestMutation
+  );
   const dispatch = useDispatch();
   const search = useInputValidation("");
-  const addFriendHandler = (id) => {
-    console.log(id);
-  };
-  const [users, setUsers] = useState([]);
-  const searchCloseHandler = () => dispatch(setIsSearch(false));
 
+  const [users, setUsers] = useState([]);
+
+  const searchCloseHandler = () => dispatch(setIsSearch(false));
+  const addFriendHandler = async (id) => {
+    await sendFriendRequest("Sending friend request", { userId: id });
+  };
   useEffect(() => {
     const timeOutId = setTimeout(() => {
       searchUser(search.value)
@@ -56,7 +64,7 @@ const Search = () => {
         ></TextField>
         <List>
           {users.map((i) => (
-            <UserItem user={i} key={i._id} handler={addFriendHandler} />
+            <UserItem user={i} key={i._id} handler={addFriendHandler} handlerIsLoading={isLoadingSendFriendRequest} />
           ))}
         </List>
       </Stack>
