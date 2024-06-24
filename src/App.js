6e6e51ac633e762bcfd2654023d642e238cff61c
Jashhmehta <jashmehtaa@gmail.com>
2024-06-24 12:29:05 +0530

    Fix: Added a fix in routes
    Replaced Id with chatId in route.

--- src/App.js
+++ src/App.js
@@ -49,7 +49,7 @@ function App() {
             }
           >
             <Route path="/" element={<Home />} />
-            <Route path="/chat/:id" element={<Chat />} />
+            <Route path="/chat/:chatId" element={<Chat />} />
             <Route path="/groups" element={<Groups />} />
           </Route>
 
