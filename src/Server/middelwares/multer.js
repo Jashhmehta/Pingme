ff494fc110a7a8d17803f8384b57e46b758d706d
Jashhmehta <jashmehtaa@gmail.com>
2024-06-24 23:49:22 +0530

    Tweak: Increased the limit of file
    - Changed the limit of file from 5mb to 500mb

--- src/Server/middelwares/multer.js
+++ src/Server/middelwares/multer.js
@@ -1,10 +1,12 @@
 import multer from "multer";
+
 const multerUpload = multer({
   limits: {
-    fileSize: 1024 * 1024 * 5,
+    fileSize: 1024 * 1024 * 500, // 5MB limit per file
   },
 });
 
-const singleAvatar=multerUpload.single("avatar");
-const attachments=multerUpload.array("files", 10);
-export {singleAvatar, attachments}
+const singleAvatar = multerUpload.single("avatar");
+const attachments = multerUpload.array("files", 10); // Max 10 files
+
+export { singleAvatar, attachments };
