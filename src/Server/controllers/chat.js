06546412e9f9a3cbd27dfbeb3e952ac8b27eb6b7
Jashhmehta <jashmehtaa@gmail.com>
2024-06-29 12:06:03 +0530

    Fix: Added security

--- src/Server/controllers/chat.js
+++ src/Server/controllers/chat.js
@@ -289,6 +289,7 @@ const renameGroup = TryCatch(async (req, res, next) => {
 });
 
 const deleteChat = TryCatch(async (req, res, next) => {
+  
   const chatId = req.params.id;
   const chat = await Chat.findById(chatId);
   if (!chat) return next(new ErrorHandler("Chat not found", 404));
@@ -325,17 +326,24 @@ const deleteChat = TryCatch(async (req, res, next) => {
 });
 
 const getMessages = TryCatch(async (req, res, next) => {
-  const chat = Chat.findById(chatId);
-  if (!chat.members.includes(req.user.toString()))
-    return next(
-      new ErrorHandler("You are not allowed to access this chat", 403)
-    );
   const chatId = req.params.id;
-  const { page = 1 } = req.query;
-  const limit = 20;
-  const skip = (page - 1) * limit;
 
   try {
+    const chat = await Chat.findById(chatId);
+    if (!chat) {
+      return next(new ErrorHandler("Chat not found", 404));
+    }
+
+    if (!chat.members.includes(req.user.toString())) {
+      return next(
+        new ErrorHandler("You are not allowed to access this chat", 403)
+      );
+    }
+
+    const { page = 1 } = req.query;
+    const limit = 20;
+    const skip = (page - 1) * limit;
+
     const [messages, totalMessagesCount] = await Promise.all([
       Message.find({ chat: chatId })
         .sort({ createdAt: -1 })
@@ -358,6 +366,7 @@ const getMessages = TryCatch(async (req, res, next) => {
     return next(new ErrorHandler("Error fetching messages", 500));
   }
 });
+
 export {
   addMembers,
   deleteChat,
