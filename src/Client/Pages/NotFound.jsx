a9fab5c2684c5d134e4368745fc25a61c22f4811
Jashhmehta <jashmehtaa@gmail.com>
2024-06-29 18:15:23 +0530

    Tweak: Added an Not found page

--- src/Client/Pages/NotFound.jsx
+++ src/Client/Pages/NotFound.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import { Error } from "@mui/icons-material";
+import { Container, Stack, Typography } from "@mui/material";
+import React from "react";
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
-  )
-}
+    <Container
+      maxWidth="lg"
+      sx={{
+        height: "100vh",
+      }}
+    >
+      <Stack>
+        <Error />
+        <Typography variant="h1">404</Typography>
+        <Typography variant="h2">Page not found</Typography>
+        <Link to="/">Go back to home page</Link>
+      </Stack>
+    </Container>
+  );
+};
 
-export default NotFound
+export default NotFound;
