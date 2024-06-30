0d29cbe6d4aa9854a99f305d6b71f9da2ce42e65
Jashhmehta <jashmehtaa@gmail.com>
2024-06-30 14:14:49 +0530

    Tweak: Modified not found page

--- src/Client/Pages/NotFound.jsx
+++ src/Client/Pages/NotFound.jsx
@@ -11,7 +11,12 @@ const NotFound = () => {
         height: "100vh",
       }}
     >
-      <Stack alignItems={"center"} spacing={"2rem"} justifyContent={"center"} height={"100%"}>
+      <Stack
+        alignItems={"center"}
+        spacing={"2rem"}
+        justifyContent={"center"}
+        height={"100%"}
+      >
         <Error />
         <Typography variant="h1">404</Typography>
         <Typography variant="h2">Page not found</Typography>
